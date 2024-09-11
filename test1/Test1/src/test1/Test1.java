8d31adf3344e4de439b9571dd356ddbcfaa157f0
ObadiahMali <157554528+ObadiahMali@users.noreply.github.com>
2024-09-11 11:19:39 +0300

    Add files via upload

---
+++ test1/Test1/src/test1/Test1.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package test1;
+public class Test1 {
+    int job;
+    
+     static double age =50;
+    
+    public static void main(String[] args) {
+      int yearsWorked =15;
+       Test1 details=new Test1();
+         System.out.println("Static =" +Test1.age);
+            System.out.println("Local  =" +yearsWorked);
+               System.out.println("Static =" +"Job variable");
+    }
+    
+}
